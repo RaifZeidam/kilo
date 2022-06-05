682f9c1ec04acc2c62b36ec171be98707b7757ac
Raif Zeidam <mrzeidam2@gmail.com>
2022-06-05 08:45:55 -0300

    projeto iniciado

---
+++ backend/src/test/java/com/equipef/kilo/KiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipef.kilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
